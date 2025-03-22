40ba5d9413dd764e14d5a36fb4d574a41ce71df1
Shadman Sohel <shadman.sohel04@gmail.com>
2025-03-22 14:52:49 -0400

    tracker

--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -1,9 +1,43 @@
+import { useEffect } from "react";
 import FooterComp from "../components/footerComp";
 import HeaderComp from "../components/headerComp";
 import LandingPageFace from "../components/landingPageFace";
 import LandingPageRestComp from "../components/landingPageRest";
 
 export default function LandingPage(){
+
+    useEffect(() => {
+        const hasVisited = localStorage.getItem('hasVisited');
+        if (!hasVisited) {
+          alert("okok");
+          sendVisitMessage();
+          localStorage.setItem('hasVisited', 'true');
+        }
+      }, []);
+      
+    const sendVisitMessage = async () => {
+        const email = "BLANK@gmail.com";
+        const message = "JUST WENT ON";
+        const subject = "I WONDER WHO?";
+
+        try {
+            const response = await fetch("https://rateto-backend.onrender.com/contact", {
+                method: "POST",
+                headers: {
+                "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    info: {
+                        email,
+                        message,
+                        subject
+                    },
+                }),
+            });
+        }
+        catch (error) {}
+      };
+    
     return(
         <>
             <HeaderComp />
