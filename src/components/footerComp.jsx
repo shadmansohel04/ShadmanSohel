e1424eb323394ce41fd84f0fa4b42be90b9a01d1
Shadman Sohel <shadman.sohel04@gmail.com>
2025-04-27 19:14:41 -0400

    updated image

--- src/components/footerComp.jsx
+++ src/components/footerComp.jsx
@@ -1,10 +1,11 @@
 import "../styles/footer.css"
+import linkedInImage from "../assets/linkedin.png"
 
 export default function FooterComp(){
     return(
         <footer className="footerComp">
             <a href="mailto:shadman.sohel04@gmail.com"><img src="./EMAIL.png" alt="" /></a>
-            <a href="https://www.linkedin.com/in/shadman-sohel/"><img style={{height: "90%", width: "90%"}} src="https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/linkedin-app-white-icon.png" alt="" /></a>
+            <a href="https://www.linkedin.com/in/shadman-sohel/"><img style={{height: "90%", width: "90%"}} src={linkedInImage} alt="" /></a>
             <a href="https://github.com/shadmansohel04"><img src="https://img.icons8.com/m_rounded/512/FFFFFF/github.png" alt="githubImg" /></a>
         </footer>
     )
