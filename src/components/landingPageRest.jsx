5a8f8be374cf130b01b86211b13c17cd486b6785
Shadman Sohel <shadman.sohel04@gmail.com>
2025-09-04 22:00:10 -0400

    added biteME

--- src/components/landingPageRest.jsx
+++ src/components/landingPageRest.jsx
@@ -6,7 +6,7 @@ export default function LandingPageRestComp(){
     const projArray = [
         {
             img: "./BITEME.png",
-            link: "LINK",
+            link: "",
             name: "BiteMe"
         },
         {
